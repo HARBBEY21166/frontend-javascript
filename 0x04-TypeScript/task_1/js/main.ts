8ad25c67f612fa9e0c341e1e00511571bccafb59
ABBEY AINA <olamilekansunday445@gmail.com>
2024-12-15 10:58:53 +0000

    task 2

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,13 +7,16 @@ interface Teacher {
     [propName: string]: any;
 }
 
-// Example usage
-const teacher3: Teacher = { 
-    firstName: 'John', 
-    fullTimeEmployee: false, 
-    lastName: 'Doe', 
-    location: 'London', 
-    contract: false 
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17
 };
 
-console.log(teacher3);
+console.log(director1);
